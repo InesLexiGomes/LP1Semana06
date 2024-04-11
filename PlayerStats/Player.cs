8b018cec917fc141a75ec72c322abee003b2a26e
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-04-12 00:50:00 +0100

    Created Player plass and started implementing values and properties

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames
+        public float highScore
+        {
+            private set {
+                if (value > highScore)
+                {
+                    highScore = value;
+                }
+            }
+        }
+    }
+}
