8a50a8923aab110da4ca4fd170ad383cbfc716f8
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-04-12 00:54:13 +0100

    added some comments to the code.

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -7,12 +7,19 @@ namespace PlayerStats
 {
     public class Player
     {
+        // Variables
         private float highScore;
         private int playedGames;
         private int wonGames
+
+        // Property that manages the high score
         public float highScore
         {
-            private set {
+            // Returns current high score
+            get { return highScore; }
+            // Check if value is greater than current value
+            private set
+            {
                 if (value > highScore)
                 {
                     highScore = value;
