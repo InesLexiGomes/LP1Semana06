5bd25ca9df87f72eb7653ca4270cc06224a2f61b
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-04-11 21:04:20 +0100

    Created colors and spheres on the program class

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,7 +6,15 @@ namespace ColorSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creates new colors
+            Color red = new Color(255, 0, 0);
+            Color green = new Color(0, 255, 0);
+            Color yellow = new Color(255, 255, 0);
+
+            // Creates new spheres
+            Sphere monkey = new Sphere(red, 20);
+            Sphere faultlessDefense = new Sphere(green, 40);
+            Sphere YRC = new Sphere(yellow, 10);
         }
     }
 }
